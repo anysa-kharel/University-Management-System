3261681902ca69b5d0f96b005a6e9a7bcf72f7f2
amritgiri <amritgiri5813@gmail.com>
2023-08-05 22:35:06 +0545

    function Student_list and student_detail

--- backend/application/urls.py
+++ backend/application/urls.py
@@ -1,21 +1,24 @@
 from django.urls import path,include
 from rest_framework import routers
-from .views import *
+from . import views
 
 router = routers.DefaultRouter()
-router.register(r'lecturer', LecturerView, basename='lecturer')
-router.register(r'student', StudentView, basename= 'student')
-router.register(r'module', ModuleView, basename = 'module')
-router.register(r'lecture', LectureView, basename='lecture')
-router.register(r'tutor', TutorView, basename = 'tutor')
-router.register(r'registration', RegistrationView, basename='registration')
-# router.register(r'department', DepartmentView, basename='department')
-router.register(r'semester', SemesterView, basename='semester')
+# router.register(r'lecturer', LecturerView, basename='lecturer')
+# router.register(r'student_list',student_list,basename='student-list')
+# router.register(r'module', ModuleView, basename = 'module')
+# router.register(r'lecture', LectureView, basename='lecture')
+# router.register(r'tutor', TutorView, basename = 'tutor')
+# router.register(r'registration', RegistrationView, basename='registration')
+# # router.register(r'department', DepartmentView, basename='department')
+# router.register(r'semester', SemesterView, basename='semester')
 
 
 urlpatterns = [
     path('',include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_famework')),
-    path('lecturer/update/<int:pk>/',LecturerView.as_view({'get': 'get', 'put': 'update', 'delete': 'delete'}), name='lecturer-detail'),
-    path('student/update/<int:pk>/',StudentView.as_view({'get': 'get', 'put': 'update', 'delete': 'delete'}), name='student-detail'),
+    # path('lecturer/update/<int:pk>/',LecturerView.as_view({'get': 'get', 'put': 'update', 'delete': 'delete'}), name='lecturer-detail'),
+    path('student/', views.student_list, name = 'student_list'),
+    path('student/<int:pk>',views.student_detail, name='student_detail'),
 ]
+    # url(r'^$', student_list),
+    # url(r'^(?P<pk>[0-9]+)', student_detail),
--- backend/application/views.py
+++ backend/application/views.py
@@ -59,35 +59,42 @@ class LecturerView(viewsets.ModelViewSet):
             lecturer.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
 
-class StudentView(viewsets.ModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+@api_view(['GET', 'POST'])
+def student_list(request):
 
-    def delete(self,request,pk = None,format = None):
-        lecturers = self.get_object(pk)
-        lecturers.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET','PUT','DELETE'])
+def student_detail(request, pk):
 
-    @api_view(['GET','POST'])
-    def student_detail(request,pk, format = None):
-        try:
-            student = Student.objects.get(pk)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        if request.method == 'GET':
-            serializer = StudentSerializer(student)
+    try:
+        part = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = StudentSerializer(part)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(part,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data)
-
-        elif request.method == 'PUT':
-            serializer =StudentSerializer(student, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        elif request.method == 'DELETE':
-            student.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        part.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ModuleView(viewsets.ModelViewSet):
