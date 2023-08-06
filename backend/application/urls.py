d3f1def59cbe003f4c27f9868b96f015ddfc6478
amritgiri <amritgiri5813@gmail.com>
2023-08-06 07:57:04 +0545

    student bug fixes

--- backend/application/urls.py
+++ backend/application/urls.py
@@ -3,23 +3,19 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'lecturer', views.LecturerView, basename='lecturer')
-# router.register(r'student',views.StudentView,basename='student')
 router.register(r'module', views.ModuleView, basename = 'module')
 router.register(r'lecture', views.LectureView, basename='lecture')
 router.register(r'tutor', views.TutorView, basename = 'tutor')
 router.register(r'registration', views.RegistrationView, basename='registration')
-# router.register(r'department', DepartmentView, basename='department')
 router.register(r'semester', views.SemesterView, basename='semester')
 
 
 urlpatterns = [
     path('',include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_famework')),
-    path('lecturer/update/<int:pk>/',views.LecturerView.as_view({'get': 'get', 'put': 'update', 'delete': 'delete'}), name='lecturer-detail'),
-    # path('student/update/<int:pk>', views.StudentView.as_view({'get': 'get', 'put': 'update', 'delete': 'delete'}), name='student-detail'),
-    path('student/update/<int:pk>',views.student_detail, name='student_detail'),
+
+    path('lecturer/',views.lecturer_list,name='lecturer_list'),
+    path('lecturer/update/<int:pk>',views.lecturer_detail, name = 'lecturer_detail'),
     path('student/', views.student_list, name = 'student_list'),
+    path('student/update/<int:pk>',views.student_detail, name='student_detail'),
 ]
-    # url(r'^$', student_list),
-    # url(r'^(?P<pk>[0-9]+)', student_detail),
